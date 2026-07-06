4b9af1828d17ba32cf3c3e361d79b25bc8e7da22
tabitha45567-lgtm <tabitha45567@gmail.com>
2026-07-06 11:12:06 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BountyAnalytics from "./BountyAnalytics.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BountyAnalytics />
+  </React.StrictMode>
+);
